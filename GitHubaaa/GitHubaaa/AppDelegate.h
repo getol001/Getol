91e753c0562d9b764be315de62f31f78e1e85646
getol001 <279869801@qq.com>
2016-09-28 14:18:17 +0800

    版本1
    描述

---
+++ GitHubaaa/GitHubaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubaaa
+//
+//  Created by Getol on 16/9/28.
+//  Copyright © 2016年 Getol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
